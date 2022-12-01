691178724ebf8103ec618810faed6f9cb574409b
lanjewarakash <lanjewarakash72@gmail.com>
2022-12-01 23:47:49 +0530

    Welcome To Snake Ladder Program

---
+++ src/com/blz/snakeladder/UC2.java
@@ -0,0 +1,12 @@
+package com.blz.snakeladder;
+
+import java.util.Random;
+
+public class UC2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int  diceRoll=random.nextInt(1,7);
+        System.out.println("current position dice is:"+diceRoll);
+    }
+}
+ 
