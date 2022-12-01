e762bd0d7b4a88a9e12ea9229c427c8b5b6e4280
lanjewarakash <lanjewarakash72@gmail.com>
2022-12-02 00:14:16 +0530

    Welcome to Snake ladder program

---
+++ src/com/blz/snakeladder/UC1.java
@@ -0,0 +1,10 @@
+package com.blz.snakeladder;
+
+public class UC1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake ladder program ");
+        int playerPosition=0;
+
+        System.out.println("current position is:"+playerPosition);
+    }
+}
